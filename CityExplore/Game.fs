fb6bcdcc2944a8aac05fbbae009e96436e00575e
Trevor Elliott <awesomelyawesome@gmail.com>
2015-10-22 22:08:49 -0700

    Start work on a simple use of IsoTiles
    Experiment with cross platform builds by having the bulk of the game reside in a library, and instantiations of it as the executables.

---
+++ CityExplore/Game.fs
@@ -0,0 +1,26 @@
+﻿module CityExplore.Game
+
+open IsoTiles.Tiles
+
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+
+type CityExploreGame() as x =
+    inherit Game()
+
+    let manager = new GraphicsDeviceManager(x)
+    let mutable sprites = Unchecked.defaultof<SpriteBatch>
+    let mutable tiles   = Unchecked.defaultof<Tiles>
+
+    override x.Initialize() =
+        sprites <- new SpriteBatch(x.GraphicsDevice)
+        base.Initialize()
+
+    override x.LoadContent() =
+        do tiles <- Tiles(x.Content, @"cityTiles_sheetl.xml")
+
+    override x.Update _ =
+        ()
+
+    override x.Draw _ =
+        x.GraphicsDevice.Clear(Color.CornflowerBlue)
